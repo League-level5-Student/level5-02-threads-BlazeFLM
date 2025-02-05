78b063a535b33786dd5329198ddbe2315837aa4e
blazi <blazi@DESKTOP-9JBQAR7>
2025-02-04 18:27:09 -0800

    6:27 PM 2/4/2025

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,86 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String args[]) {
+		int X = 400;
+		int Y = 400;
+		
+		Thread t1 = new Thread(()->{
+			Robot rob = new Robot();
+			rob.setPenWidth(10);
+			rob.setX(X);
+			rob.setY(Y);
+			rob.setPenColor(Color.blue);
+			for(int i = 0; i < 30; i++) {
+				rob.penDown();
+				rob.move(30);
+				rob.turn(360/30);
+			}
+		});
+		
+		Thread t2 = new Thread(()->{
+			Robot rob = new Robot();
+			rob.setPenWidth(10);
+			rob.setX(X + 240);
+			rob.setY(Y);
+			rob.setPenColor(Color.black);
+			for(int i = 0; i < 30; i++) {
+				rob.penDown();
+				rob.move(30);
+				rob.turn(360/30);
+			}
+		});
+		
+		Thread t3 = new Thread(()->{
+			Robot rob = new Robot();
+			rob.setPenWidth(10);
+			rob.setX(X + 480);
+			rob.setY(Y);
+			rob.setPenColor(Color.red);
+			for(int i = 0; i < 30; i++) {
+				rob.penDown();
+				rob.move(30);
+				rob.turn(360/30);
+			}
+		});
+		
+		Thread t4 = new Thread(()->{
+			Robot rob = new Robot();
+			rob.setPenWidth(10);
+			rob.setX(X + 120);
+			rob.setY(Y + 200);
+			rob.setPenColor(Color.yellow);
+			for(int i = 0; i < 30; i++) {
+				rob.penDown();
+				rob.move(30);
+				rob.turn(360/30);
+			}
+		});
+		
+		Thread t5 = new Thread(()->{
+			Robot rob = new Robot();
+			rob.setPenWidth(10);
+			rob.setX(X + 360);
+			rob.setY(Y + 200);
+			rob.setPenColor(Color.green);
+			for(int i = 0; i < 30; i++) {
+				rob.penDown();
+				rob.move(30);
+				rob.turn(360/30);
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+		
+	}
 }
-
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,40 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		int X = 400;
+		int Y = 1000;
+		Random dom = new Random();
+		
+		Thread[] thrds = new Thread[5];
+		Robot[] bots = new Robot[5];
+		
+		for(int i = 0; i < 5; i++) {
+            bots[i] = new Robot();
+        }
+		
+		for(int i = 0; i < 5; i++) {
+			int k = i;
+			thrds[i] = new Thread(()->{
+				bots[k].setX(X + (k * 100));
+				bots[k].setY(Y);
+				for(Robot r : bots) {
+					r.move(dom.nextInt(100));
+				}
+				//get program to run indefinitely until a bot crosses y = 100
+			});
+		}
+		
+		
+		for(Thread t : thrds) {
+			t.start();
+		}
+		
+	}
 }
